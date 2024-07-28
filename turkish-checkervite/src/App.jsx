01bff422146b49b7bc74bf343414b83005cad64a
Kerim <kerimcanak@gmail.com>
2024-07-28 22:14:10 +0300

    transfer completed, will remove unnecessary files

--- turkish-checkervite/src/App.jsx
+++ turkish-checkervite/src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  function check(formData) {
+    const query = formData.get("query");
+    alert(`You searched for '${query}'`);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    < >
+    <form className="flex flex-col items-center justify-center bg-black h-screen w-screen gap-8" onSubmit={check}>
+    <h3 className="text-white text-3xl">Enter text to area to check wether if the text is Turkish or not.</h3>
+    <h3 className="text-white text-3xl">Text language: fizbuzz</h3>
+    <div className="relative">
+      <div className="absolute inset-0 shadow-white shadow-2xl animate-pulse"></div>
+      <textarea name="query" type="text" className="border p-2 w-[20cm] h-[4cm] relative z-10" />
+    </div>
+    <button type="submit" className="border p-2 text-white rounded-md z-10">Check</button>
+    </form>
     </>
   )
 }
